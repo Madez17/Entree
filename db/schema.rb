d8a6a1799e66ba129359621556725c0539252403
Madez <781@holbertonschool.com>
2019-10-15 22:01:45 +0000

    [Model-reason] - create model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_213049) do
+ActiveRecord::Schema.define(version: 2019_10_15_220028) do
+
+  create_table "reasons", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
