9a79436935350c0f0a16145238a5d03c5e29eae9
naxus1 <naxus1@gmail.com>
2019-10-16 18:14:01 -0500

    [Test - Models] - Create test role and visitor

--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class RoleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Tle type will be not empty" do
+    type = Role.new(text: "")
+    assert_not type.save
+  end
+  
 end
