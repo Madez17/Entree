cd0dcc89ba0f58b7a603606ae5c51bc9c1bd357c
Jose Medina <fermed28@gmail.com>
2019-10-16 16:53:21 -0500

    [add] create test for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_and_belongs_to_many :roles
+
+    validates :email, :name, :lastname, :identification, presence: true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+    validates :email, :identification, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "email cant be blank to create" do
+      mafe = User.new(name: "mafe", lastname: "mendez", identification: 456789)      
+      assert_not mafe.save
+  end
+
+  test "email correct format to create" do
+    mafe = User.new(name: "mafe", lastname: "mendez", identification: 456789, email: "asdadsad")      
+    assert_not mafe.save  
+  end
+
+  test "name cant be blank to create" do
+    mafe = User.new(lastname: "mendez", identification: 456789, email: "a@a.com")      
+    assert_not mafe.save
+  end
+
+  test "lastname cant be blank to create" do
+    mafe = User.new(name: "mafe", identification: 456789, email: "a@a.com")      
+    assert_not mafe.save
+  end
+
+  test "identification cant be blank to create" do
+    mafe = User.new(name: "mafe", lastname: "mendez", email: "a@a.com")      
+    assert_not mafe.save
+  end
+
+  test "email should be unique to create" do
+    mafe = User.new(name: "mafe", lastname: "mendez",identification: 456789, email: "a@a.com")
+    mafe.save  
+    fer = User.new(name: "fer", lastname: "medina", identification: 456788, email: "a@a.com")
+         
+    assert_not fer.save
+  end
+
+  test "identification should be unique to create" do
+    mafe = User.new(name: "mafe", lastname: "mendez",identification: 456789, email: "a@a.com")
+    mafe.save  
+    fer = User.new(name: "fer", lastname: "medina", identification: 456789, email: "b@a.com")
+         
+    assert_not fer.save
+  end
+
 end
