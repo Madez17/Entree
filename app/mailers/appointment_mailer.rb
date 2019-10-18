9da1cdace8d44946cea47e9c90dd773fd1c55f99
Madez <781@holbertonschool.com>
2019-10-18 22:00:39 +0000

    [Mailer] - create a mailer

---
+++ app/mailers/appointment_mailer.rb
@@ -0,0 +1,9 @@
+class AppointmentMailer < ApplicationMailer
+   def appointment_email(appointment)
+    @user = appointment.user
+    @appointment = appointment
+    @visitor = appointment.visitor
+    @reason = appointment.reason
+    mail(to: @user.email, subject: 'You have a visitor :)!')
+  end 
+end
