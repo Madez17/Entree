49eda273a169809a51afdc246d06f5040cc0ebfd
Jose Medina <fermed28@gmail.com>
2019-10-17 19:58:12 -0500

    [add] autocomplete feature

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,4 +4,12 @@ class AppointmentsController < ApplicationController
         @users = User.all
         @appointment = Appointment.new
     end
+
+
+
+    def get_user
+        p "aqui estoy"
+        @users = User.select(:name, :lastname, :id).all
+        render json: @users
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'appointments#index'
+  root 'appointments#index' 
+  get 'appointments/get_user', to: 'appointments#get_user' 
   resources :appointments
+  
 end
