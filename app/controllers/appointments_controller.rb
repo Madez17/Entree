b271d104e4c0133405811f177ba132218fb096fb
Jose Medina <fermed28@gmail.com>
2019-10-21 23:54:44 -0500

    [wip] send image and store with active storage

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -15,13 +15,14 @@ class AppointmentsController < ApplicationController
                 render "create" and return
             end
        end
-       appointment = Appointment.new(appointment_parameter)
-       appointment.visitor_id = @visitor.id
-       unless appointment.save
+        appointment = Appointment.new(appointment_parameter)
+        appointment.visitor_id = @visitor.id
+        appointment.photo.attach(io: image_io, filename: 'phto_image')    
+        unless appointment.save
             @errors = appointment.errors
             render "create" and return
-       end
-    #    AppointmentMailer.appointment_email(appointment).deliver_now
+        end
+        AppointmentMailer.appointment_email(appointment).deliver_now
     end
 
     def get_users
@@ -36,5 +37,10 @@ class AppointmentsController < ApplicationController
 
     def appointment_parameter
         params.require(:appointment).permit(:reason_id, :message, :user_id)
+    end
+
+    def image_io
+        decoded_image = Base64.decode64(params[:appointment][:photo])
+        StringIO.new(decoded_image)
     end
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,5 +2,7 @@ class Appointment < ApplicationRecord
   belongs_to :reason
   belongs_to :user
   belongs_to :visitor
-  validates :message, presence: true  
+  validates :message, presence: true 
+  has_one_attached :photo 
+  validates :photo, presence: true 
 end
