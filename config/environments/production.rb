4bdd9c1343c64babb3ca5fa86da2131868047ac5
Jose Medina <fermed28@gmail.com>
2019-10-24 13:01:20 -0500

    [add] production environment for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :mailjet
+  config.action_mailer.default_url_options = { :host => "entreeapp.herokuapp.com" }
+  config.action_controller.asset_host = 'https://entreeapp.herokuapp.com/'
+  config.action_mailer.asset_host = config.action_controller.asset_host
 end
