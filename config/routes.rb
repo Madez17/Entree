2cbc9b9fccf2346d935b0eb20af26bbcadd047cd
naxus1 <naxus1@gmail.com>
2019-10-30 11:02:24 -0500

    [add] create home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  root 'home#index'
   namespace :api do
     namespace :v1 do
       resources :arrivals
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'appointments#index' 
+  #root 'appointments#index' 
 
   get 'appointments/get_users', to: 'appointments#get_users'
   resources :appointments
