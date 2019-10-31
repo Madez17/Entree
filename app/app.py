dc925371e202c7fc6fa30443890f88a74dc6c5b2
naxus1 <naxus1@gmail.com>
2019-10-30 21:14:36 -0500

    [add] conect with herokuapp

--- app/app.py
+++ app/app.py
@@ -10,8 +10,10 @@ import cv2
 from imageio import imread
 import io
 import os
-
+import requests
+URI_APP = 'http://entreeapp.herokuapp.com/api/v1/arrivals'
 app = Flask(__name__)
+cors = CORS(app, resources={r'*': {'origins': '*'}})
 app.url_map.strict_slashes = False
 
 @app.route('/status')
@@ -33,13 +35,13 @@ def login_post():
     b64_bytes = base64.b64encode(image)
     b64_string = b64_bytes.decode()
     img = imread(io.BytesIO(base64.b64decode(b64_string)))
-
+    data = {}
     # finally convert RGB image to BGR for opencv
     # and save result
     cv2_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    result = recognizer.recognize(cv2_img)
-    #create call to heroku
-    return jsonify(result)
+    data["users"] = recognizer.recognize(cv2_img)
+    response = requests.post(URI_APP, json=data).json()
+    return jsonify(response)
 
 
 if __name__ == '__main__':
